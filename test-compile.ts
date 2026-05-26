d2521177c00fad8db861ea61418e57f3e720f210
JhonEspin <jhesrka@hotmail.com>
2026-05-25 19:15:44 -0500

    chore: remove all junk scripts, debug files, html tests and logs

--- test-compile.ts
+++
@@ -1,5 +0,0 @@
-import { FinancialService } from './src/presentation/services/financial/financial.service';
-
-const service = new FinancialService();
-console.log(service.getShopReconciliation);
-console.log(service.getUnifiedTransactions);
