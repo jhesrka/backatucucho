50330a76024f4fd3a2914c1edfd907def4dcfb3f
JhonEspin <jhesrka@hotmail.com>
2026-08-04 16:01:04 -0500

    fix(creditos): use AuthAdminMiddleware for admin auditoria route

--- src/presentation/formulario-credito/formulario-credito.routes.ts
+++ src/presentation/formulario-credito/formulario-credito.routes.ts
@@ -2,8 +2,8 @@ import { Router } from "express";
 import { FormularioCreditoController } from "./formulario-credito.controller";
 import { FormularioCreditoService } from "../services/formularioCredito.service";
 import { AuthMiddleware } from "../../middlewares/auth.middleware";
-import { UserRole } from "../../data/postgres/models/user.model";
-
+import { AuthAdminMiddleware } from "../../middlewares/auth-admin.middleware";
+import { UserRoleAdmin } from "../../data/postgres/models/useradmin.model";
 export class FormularioCreditoRoutes {
   static get routes(): Router {
     const router = Router();
@@ -23,7 +23,7 @@ export class FormularioCreditoRoutes {
     router.post('/pagar-lead', AuthMiddleware.protect, controller.pagarLeadCredito);
 
     // Auditoría de Leads (Solo Admin)
-    router.get('/auditoria/:codigo', AuthMiddleware.protect, AuthMiddleware.restrictTo(UserRole.ADMIN), controller.obtenerLeadPorCodigoAuditoria);
+    router.get('/auditoria/:codigo', AuthAdminMiddleware.protect, AuthAdminMiddleware.restrictTo(UserRoleAdmin.ADMIN), controller.obtenerLeadPorCodigoAuditoria);
 
     return router;
   }
